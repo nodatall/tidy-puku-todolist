1f2ca9d9ef536df8a391955c5e2c111e86285e46
Sylvan Swierkosz <sswierkosz@gmail.com>
2016-10-31 11:31:33 -0700

    Install dependencies | Create scripts

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: './dist/index.js'
+  },
+  module: {
+    loaders: [
+      test: /\.jsx?$/,
+      loader: 'babel',
+      exclude: /(node_modules)/,
+      query: {
+        presets: ['es2015']
+      }
+    ]
+  }
+}

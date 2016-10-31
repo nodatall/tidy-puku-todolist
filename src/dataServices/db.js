72804d8b092d55a98250f25a06b8216794985755
Sylvan Swierkosz <sswierkosz@gmail.com>
2016-10-31 14:45:08 -0700

    Add database query functions

---
+++ src/dataServices/db.js
@@ -0,0 +1,65 @@
+const pgp = require('pg-promise')()
+const connectionString = 'postgres://localhost:5432/taskDatabase'
+const db = pgp(connectionString)
+
+const getAll = () => {
+  const sql = `SELECT * FROM tasks`
+  return db.any(sql)
+}
+
+const add = (text) => {
+  const sql = `
+    INSERT INTO tasks (text)
+    VALUES ($1)
+  `
+  return db.none(sql, [text])
+}
+
+const markAsComplete = (id) => {
+  const sql = `
+    UPDATE tasks
+    SET completed = true
+    WHERE id = $1
+  `
+  return db.none(sql, [id])
+}
+
+const markAsIncomplete = (id) => {
+  const sql = `
+    UPDATE tasks
+    SET completed = false
+    WHERE id = $1
+  `
+  return db.none(sql, [id])
+}
+
+const edit = (id, text) => {
+  const sql = `
+    UPDATE tasks
+    SET text = $2
+    WHERE id = $1
+  `
+  return db.none = (sql, [id, text])
+}
+
+const delete = (id) => {
+  const sql = `
+    DELETE FROM tasks
+    WHERE id = $1
+  `
+  return db.none = (sql, [id])
+}
+
+const reorder = (id) => {
+
+}
+
+module.exports = {
+  getAll,
+  add,
+  markAsComplete,
+  markAsIncomplete,
+  edit,
+  remove,
+  reorder,
+}
--- src/routes.js
+++ src/routes.js
@@ -6,8 +6,9 @@ const taskController = require('./controllers/taskController')
 router.get('/', taskController.getAll)
 router.post('/add', taskController.add)
 router.put('/complete/:id', taskController.markAsComplete)
-router.put('/uncomplete/:id'), taskController.markAsUncomplete)
+router.put('/uncomplete/:id'), taskController.markAsIncomplete)
 router.put('/edit/:id', taskController.edit)
+router.put('/reorder', taskController.reorder)
 router.delete('/delete/:id', taskController.remove)
 
 module.exports = router

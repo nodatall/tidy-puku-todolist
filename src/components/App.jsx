6e32f5ebd026490361281c98c5f1a3e10f9a3773
Sylvan Swierkosz <sswierkosz@gmail.com>
2016-11-02 14:02:31 -0700

    Replace react-addons-css-transition-group with react-css-transition-replace

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import TaskList from './TaskList.jsx'
 import $ from 'jquery'
-import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+import ReactCSSTransitionReplace from 'react-css-transition-replace'
 
 class App extends React.Component {
 
@@ -50,37 +50,36 @@ class App extends React.Component {
 
     return (
       <div className='container'>
-
-          {this.state.showCompletedFlag ?
-            <div className='back topButton' onClick={this.showIncompleted.bind(this)}>
-              <svg width="28" height="28">
-                <path d="M 11, 6 L 3, 14 L 11, 22" fill="none"/>
-                <line x1="4" y1="14" x2="25" y2="14" />
-              </svg>
-            </div> :
-            <div className='buttonContainer'>
-              <div className='add topButton' onClick={this.addTask.bind(this)}>
+        <ReactCSSTransitionReplace
+          transitionName="cross-fade"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={500} >
+          <div key={`${this.state.showCompletedFlag}1234`}>
+            {this.state.showCompletedFlag ?
+              <div className='back topButton' onClick={this.showIncompleted.bind(this)}>
                 <svg width="28" height="28">
-                  <line x1="14" y1="3" x2="14" y2="25" />
-                  <line x1="3" y1="14" x2="25" y2="14" />
+                  <path d="M 11, 6 L 3, 14 L 11, 22" fill="none"/>
+                  <line x1="4" y1="14" x2="25" y2="14" />
                 </svg>
+              </div> :
+              <div className='buttonContainer'>
+                <div className='add topButton' onClick={this.addTask.bind(this)}>
+                  <svg width="28" height="28">
+                    <line x1="14" y1="3" x2="14" y2="25" />
+                    <line x1="3" y1="14" x2="25" y2="14" />
+                  </svg>
+                </div>
+                <div className='showCompleted topButton' onClick={this.showCompleted.bind(this)}>
+                  <svg width="28" height="28">
+                    <circle cx="14" cy="14" r="12" fill="none"/>
+                    <path id="check" d="M 6, 15 L 12, 21 L 20, 9" fill="none"/>
+                  </svg>
+                </div>
               </div>
-              <div className='showCompleted topButton' onClick={this.showCompleted.bind(this)}>
-                <svg width="28" height="28">
-                  <circle cx="14" cy="14" r="12" fill="none"/>
-                  <path id="check" d="M 6, 15 L 12, 21 L 20, 9" fill="none"/>
-                </svg>
-              </div>
-            </div>
-          }
-
-          {taskList}
-        <ReactCSSTransitionGroup
-          transitionName="example"
-          transitionEnterTimeout={1000}
-          transitionLeaveTimeout={500}>
-        {this.state.showCompletedFlag ? <h1 key='109858'>One</h1> : <h1 key='5445454'>Two</h1>}
-        </ReactCSSTransitionGroup>
+            }
+            {taskList}
+          </div>
+        </ReactCSSTransitionReplace>
       </div>
     )
   }
